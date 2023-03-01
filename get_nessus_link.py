d1ef11de213c92c7cc1fd96a8e402bf23757e1ab
Manraj Singh <manraj.singh@hcai.ca.gov>
2023-03-01 12:43:12 -0800

    Version update
    - Fixes the 404 error (by updating download link) referencing previous version link
    - Updated get_nessus_link.py to output b2sum of new file
    - Added requirements.txt for quick python env for `get_nessus_link.py`
    	Create python virtual env  `virtualenv .env`
    	Activate virtual env `source .env/bin/activate`
    	install dependencies `pip install -r requirements.txt`
    	run `python get_nessus_link.py` to get updated link and b2sum that is to be used in `.SRCINFO` and `PKGBUILD` files.

--- get_nessus_link.py
+++ get_nessus_link.py
@@ -7,7 +7,8 @@
 #   pacman -S python python-beautifulsoup4 python-requests
 
 import json
-
+import hashlib
+import os
 from bs4 import BeautifulSoup
 import requests
 
@@ -19,11 +20,33 @@ def main():
         filename = download['file']
         if filename.endswith('-fc34.x86_64.rpm'):
             url = f'https://www.tenable.com/downloads/api/v1/public/pages/nessus-agents/downloads/{download["id"]}/download?i_agree_to_tenable_license_agreement=true'
+            print(f'Downloading: {filename}')
+            r = requests.get(url, allow_redirects=True)
+
+            with open(filename, 'wb') as bfile:
+                _f = bfile.write(r.content)
+            
+            with open(filename, 'rb') as bfile:
+                blake_hash_agent = hashlib.blake2b()
+                for chunk in iter(lambda: bfile.read(4096), b""):
+                    blake_hash_agent.update(chunk)
+
+            with open('LICENSE', 'rb') as bfile:
+                blake_hash_license = hashlib.blake2b()
+                for chunk in iter(lambda: bfile.read(4096), b""):
+                    blake_hash_license.update(chunk)
+            
+            os.remove(filename)
+            
             break
 
     if url is not None:
-        print(filename)
-        print(url)
+        print(f'\nFile: {filename}')
+        print(f'URL: {url}')
+        print(f"\nb2sums=('{blake_hash_agent.hexdigest()}'\n\t'{blake_hash_license.hexdigest()}')\n")
+
+
+
     else:
         print('Cannot find a download link!')
 
